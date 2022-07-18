aa34d92c2026bbb74296285c724c89678e6be8f5
Clemens Winter <clemenswinter1@gmail.com>
2022-07-17 17:09:47 -0700

    Derive macro for Featurizable

---
+++ entity-gym-rs-derive/src/lib.rs
@@ -0,0 +1,103 @@
+use proc_macro2::TokenStream;
+use proc_macro_error::{abort, proc_macro_error};
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Field, Type};
+
+#[proc_macro_error]
+#[proc_macro_derive(Featurizable)]
+pub fn derive_featurizable(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = &input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let (num_feats, features_names, featurize) = field_names(&input.data);
+    let name_str = name.to_string();
+
+    // Build the output, possibly using quasi-quotation
+    let expanded = quote! {
+        #[allow(unused_qualifications)]
+        impl #impl_generics Featurizable for #name #ty_generics #where_clause {
+            fn num_feats() -> usize {
+                #num_feats
+            }
+
+            fn feature_names() -> &'static [&'static str] {
+                #features_names
+            }
+
+            fn featurize(&self) -> Vec<f32> {
+                #featurize
+            }
+
+            fn name() -> &'static str {
+                #name_str
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler
+    proc_macro::TokenStream::from(expanded)
+}
+
+fn field_names(data: &Data) -> (TokenStream, TokenStream, TokenStream) {
+    match data {
+        Data::Struct(data) => match &data.fields {
+            syn::Fields::Named(fields) => {
+                let mut names = vec![];
+                let count = fields.named.len();
+                let mut features = vec![];
+                for field in fields.named.iter() {
+                    names.push(field.ident.as_ref().unwrap().to_string());
+                    features.push(field_to_f32(field))
+                }
+
+                (
+                    quote! { #count },
+                    quote! { &[#(#names),*] },
+                    quote! { vec![#(#features),*] },
+                )
+            }
+            syn::Fields::Unnamed(_fields) => todo!(),
+            // syn::Fields::Unnamed(fields) => {
+            //     let mut names = vec![];
+            //     for i in 0..fields.unnamed.len() {
+            //         let name = format!("_{}", i);
+            //         names.push(quote! { #name });
+            //     }
+            //     let count = fields.unnamed.len();
+            //     (quote! { #count }, quote! { &[#(#names),*] })
+            // }
+            syn::Fields::Unit => (quote! { 0 }, quote! { &[] }, quote! { ::std::Vec::new() }),
+        },
+        Data::Enum(_) => unimplemented!("Enum not supported, must be struct"),
+        Data::Union(_) => unimplemented!("Union not supported, must be struct"),
+    }
+}
+
+fn field_to_f32(field: &Field) -> TokenStream {
+    let ident = field.ident.as_ref().unwrap();
+    match &field.ty {
+        Type::Path(ty) => {
+            let ty = ty.path.segments.last().unwrap();
+            match ty.ident.to_string().as_str() {
+                "f32" => quote! { self.#ident },
+                "f64" | "u8" | "u16" | "u32" | "u64" | "i8" | "i16" | "i32" | "i64" => {
+                    quote! { self.#ident as f32 }
+                }
+                "bool" => quote! { if self.#ident { 1.0 } else { 0.0 } },
+                _ => abort!(
+                    ty,
+                    "Can't derive Featurizable for type `{}`",
+                    quote!(#ty).to_string(),
+                ),
+            }
+        }
+        t => abort!(
+            t,
+            "Can't derive Featurizable for type `{}`",
+            quote!(#t).to_string(),
+        ),
+    }
+}
--- src/agent/featurizable.rs
+++ src/agent/featurizable.rs
@@ -22,3 +22,55 @@ impl<'a, T: Featurizable> Featurizable for &'a T {
         T::name()
     }
 }
+
+mod test {
+    use super::*;
+
+    use entity_gym_rs_derive::Featurizable;
+
+    #[derive(Featurizable)]
+    struct Pos {
+        x: f32,
+        y: f32,
+    }
+
+    #[derive(Featurizable)]
+    struct Hero {
+        x: f32,
+        y: f32,
+        level: u32,
+        alive: bool,
+    }
+
+    #[test]
+    fn test_num_feats() {
+        assert_eq!(Pos::num_feats(), 2);
+        assert_eq!(Hero::num_feats(), 4);
+    }
+
+    #[test]
+    fn test_feature_names() {
+        assert_eq!(Pos::feature_names(), &["x", "y"]);
+        assert_eq!(Hero::feature_names(), &["x", "y", "level", "alive"]);
+    }
+
+    #[test]
+    fn test_featurize() {
+        assert_eq!(Pos::featurize(&Pos { x: 1.0, y: 2.0 }), vec![1.0, 2.0]);
+        assert_eq!(
+            Hero::featurize(&Hero {
+                x: 1.0,
+                y: 2.0,
+                level: 3,
+                alive: true,
+            }),
+            vec![1.0, 2.0, 3.0, 1.0]
+        );
+    }
+
+    #[test]
+    fn test_name() {
+        assert_eq!(Pos::name(), "Pos");
+        assert_eq!(Hero::name(), "Hero");
+    }
+}
